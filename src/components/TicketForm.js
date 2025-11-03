a89ef5cc12097d43c7e5bd5981f2e1d8447f1651
Candido <thiagocandido.bending@gmail.com>
2025-11-03 17:01:13 -0300

    Blaster - Bug reporting App (React)

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function TicketForm() {
+export default function TicketForm({ dispatch, editingTicket }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [priority, setPriority] = useState("1");
+  const [priority, setPriority] = useState(1);
+
+  useEffect(() => {
+    console.log("💡 useEffect fired:", editingTicket);
+    if (editingTicket) {
+      setTitle(editingTicket.title);
+      setDescription(editingTicket.description);
+      setPriority(editingTicket.priority);
+    } else {
+      clearForm();
+    }
+  }, [editingTicket]);
 
   const priorityLabels = {
     1: "Low",
@@ -14,16 +25,36 @@ export default function TicketForm() {
   const clearForm = () => {
     setTitle("");
     setDescription("");
-    setPriority("1");
+    setPriority(1);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const ticketData = {
+      id: editingTicket ? editingTicket.id : new Date().toISOString(),
+      title,
+      description,
+      priority: Number(priority),
+    };
+
+    dispatch({
+      type: editingTicket ? "UPDATE_TICKET" : "ADD_TICKET",
+      payload: ticketData,
+    });
+
+    console.log(ticketData);
+
+    clearForm();
+  };
+
+  const handleCancelEdit = () => {
+    dispatch({ type: "CLEAR_EDITING_TICKET" });
     clearForm();
   };
 
   return (
-    <form onSubmite={handleSubmit} className="ticket-form">
+    <form onSubmit={handleSubmit} className="ticket-form">
       <div>
         <label>Title</label>
         <input
@@ -42,6 +73,33 @@ export default function TicketForm() {
           onChange={(e) => setDescription(e.target.value)}
         />
       </div>
+
+      <fieldset className="priority-fieldset">
+        <legend>priority</legend>
+
+        {Object.entries(priorityLabels).map(([value, label]) => (
+          <label key={value} className="priority-label">
+            <input
+              type="radio"
+              value={value}
+              checked={priority === value}
+              className="priority-input"
+              onChange={(e) => setPriority(Number(e.target.value))}
+            ></input>
+            {label}
+          </label>
+        ))}
+      </fieldset>
+
+      <button type="submit" className="button">
+        Submit
+      </button>
+
+      {editingTicket && (
+        <button className="button" onClick={handleCancelEdit}>
+          Cancel Edit
+        </button>
+      )}
     </form>
   );
 }
